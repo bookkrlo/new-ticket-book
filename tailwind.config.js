4d0d5093f5d4d9c8d53ce503ea856701529a3404
bookkrlo <bookkrlo.info@gmail.com>
2024-11-15 04:31:30 +0500

    primary colors added tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,16 @@ module.exports = {
   theme: {
   	extend: {
   		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
+  			primary: {
+          100: '#f5ffe3',   // Light shade
+          200: '#e2ffaa',  // Lighter shade
+          300: '#ceff71',        // Base color
+          400: '#baff39',   // Darker shade
+          500: '#a7ff00',   // Dark shade
+          600: '#82c600',   // Darker shade
+          700: '#5d8e00', // Even darker
+          800: '#385500', // Very dark
+          900: '#131c00', // Darkest
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
