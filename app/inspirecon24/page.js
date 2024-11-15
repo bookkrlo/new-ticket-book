dac9fc5e1dee74a97851aa59891179096c0fe419
bookkrlo <bookkrlo.info@gmail.com>
2024-11-15 05:21:53 +0500

    event time update page.js

--- app/inspirecon24/page.js
+++ app/inspirecon24/page.js
@@ -321,7 +321,7 @@ export default function Component() {
                             </div>
                             <div className='flex items-center'>
                                 <Clock className='w-5 h-5 mr-2' />
-                                <span>08:30 pm</span>
+                                <span>11:00 pm</span>
                             </div>
                             <div className='flex items-center'>
                                 <MapPin className='w-5 h-5 mr-2' />
