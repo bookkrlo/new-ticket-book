2a148d238aa59a069e16bb5b5fb914c7fa12bd4e
bookkrlo <bookkrlo.info@gmail.com>
2024-11-18 18:31:07 +0500

    added meta fb code in inspirecon page layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,26 @@ export default function RootLayout({ children }) {
     return (
         <html lang='en'>
         <head>
+
+        <!-- Meta Pixel Code -->
+    <script>
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '943134844582031');
+                fbq('track', 'PageView');
+                </script>
+                <noscript><img height="1" width="1" style="display:none"
+                src="https://www.facebook.com/tr?id=943134844582031&ev=PageView&noscript=1"
+                />
+    </noscript>
+    <!-- End Meta Pixel Code -->
+        
         <title>Bookkrlo - Effortless Ticket Booking</title>
         <meta
           name="description"
@@ -19,7 +39,7 @@ export default function RootLayout({ children }) {
           property="og:description"
           content="Events Near or Far! Just Book Krlo, Yaar!"
         />
-        <meta property="og:image" content="/assets/images/logo.svg" />
+        <meta property="og:image" content="@/assets/images/logo.svg" />
         <meta property="og:url" content="https://bookkrlo.com" />
       </head>
               
