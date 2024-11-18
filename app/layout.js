3e2f3a9ad9d9d38f5e73e9a5f4d8d341a2c86113
bookkrlo <bookkrlo.info@gmail.com>
2024-11-18 17:56:30 +0500

    Update layout.js main page Header SEO layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,28 @@
 import './globals.css';
 import { Toaster } from 'sonner';
 
-export const metadata = {
-    title: 'Book Krlo',
-    description: 'Ticket booking website',
-};
+
 
 export default function RootLayout({ children }) {
     return (
         <html lang='en'>
+        <head>
+        <title>Bookkrlo - Effortless Ticket Booking</title>
+        <meta
+          name="description"
+          content="Book tickets for events, movies, and travel easily with Bookkrlo."
+        />
+        <meta name="keywords" content="bookkrlo, book karlo, book krlo, bookkarlo.com, bookkrlo.com ,tickets, events, movies, travel" />
+        <meta name="author" content="Bookkrlo Team" />
+        <meta property="og:title" content="Bookkrlo - Book Tickets Online" />
+        <meta
+          property="og:description"
+          content="Events Near or Far! Just Book Krlo, Yaar!"
+        />
+        <meta property="og:image" content="/assets/images/logo.svg" />
+        <meta property="og:url" content="https://bookkrlo.com" />
+      </head>
+              
             <body>
                 {children}
                 <Toaster />
