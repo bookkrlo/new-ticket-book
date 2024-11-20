555fa8e060ff3068e17851b977cb483a1f3b385b
bookkrlo <bookkrlo.info@gmail.com>
2024-11-20 11:06:11 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,48 @@
 import './globals.css';
 import { Toaster } from 'sonner';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-
+{/*
+        <meta
+          name="description"
+          content="Book tickets for events, movies, and travel easily with Bookkrlo."
+        />
+        <meta name="keywords" content="bookkrlo, book karlo, book krlo, bookkarlo.com, bookkrlo.com ,tickets, events, movies, travel" />
+        <meta name="author" content="Bookkrlo Team" />
+        <meta property="og:title" content="Bookkrlo - Book Tickets Online" />
+        <meta
+          property="og:description"
+          content="Events Near or Far! Just Book Krlo, Yaar!"
+        />
+        <meta property="og:image" content="https://res.cloudinary.com/di0zgxtka/image/upload/v1732079706/book_kr_lo_logo_500_x_500_lohroq.webp" />
+        <meta property="og:url" content="https://bookkrlo.com" />
+*/}
 
 export default function RootLayout({ children }) {
+     const router = useRouter();
+
+    {/* Default meta tags (Main page)  */}
+  const defaultMeta = {
+    title: "Bookkrlo - Book and Sell Tickets for Events, Movies, and Travel",
+    description: "Explore and book tickets for events, movies, and travel. Your one-stop platform for easy ticketing.",
+    image: "https://res.cloudinary.com/di0zgxtka/image/upload/v1732079706/book_kr_lo_logo_500_x_500_lohroq.webp",
+    url: "https://bookkrlo.com",
+  };
+
+    {/* Custom meta tags for specific routes */}
+  const meta = router.pathname === "/inspirecon24"
+    ? {
+        title: "InspireCon'24 - Empowering Entrepreneurs",
+        description:
+          "Join the biggest entrepreneurial event of the year. Gain insights from renowned speakers and network with like-minded individuals.",
+        image: "https://res.cloudinary.com/di0zgxtka/image/upload/v1732079438/inspirecon-dropdown-banner_sgrdmj.webp",
+        url: "https://bookkrlo.com/inspirecon24",
+      }
+    : defaultMeta;
+
+
+    
     return (
         <html lang='en'>
         <head>
@@ -23,7 +62,13 @@ export default function RootLayout({ children }) {
             }),
           }}
         />
-        <title>Bookkrlo - Effortless Ticket Booking</title>
+            
+         <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:image" content={meta.image} />
+        <meta property="og:url" content={meta.url} />
        
 
               {/* <script>
